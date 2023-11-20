3b822c18030dc8ef47c9cb9b902da784355c725e
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2023-11-20 12:28:04 +0800

    Update MusicPlayerSubstate.hx

--- source/MusicPlayerSubstate.hx
+++ source/MusicPlayerSubstate.hx
@@ -15,11 +15,11 @@ import flixel.tweens.FlxTween;
 import flixel.math.FlxMath;
 import openfl.geom.Point;
 import flixel.system.FlxSound;
+import Sys;
+import sys.FileSystem;
 
 #if windows
 import Discord.DiscordClient;
-import Sys;
-import sys.FileSystem;
 #end
 
 class MusicPlayerSubstate extends MusicBeatSubstate
